8cf8d4327b6b6a72645fe609dab187ae3c750a41
ssgdplw <66963618+ssgdplw@users.noreply.github.com>
2025-06-11 21:21:02 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -29,9 +29,7 @@ PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-adblock-fast-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
